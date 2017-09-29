1063f71208519c775cb446b9d40e50644f87d19c
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-29 02:52:43 +0000

    automatically backed up by learn

--- lib/class_variable.rb
+++ lib/class_variable.rb
@@ -1,12 +1,16 @@
 class Album
- 
+
   @@album_count = 0
- 
+
   def release_date=(date)
     @release_date = date
   end
- 
+
   def release_date
     @release_date
+  end
+
+  def self.count
+    @@album_count
   end
 end
